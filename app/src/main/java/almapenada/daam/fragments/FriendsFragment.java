380027b1da8d40a8dda8130fce47f14046a84f84
André Carvalho <andrempc@gmail.com>
2016-03-19 22:17:44 +0000

    Friends Tabs (friends and groups)

--- app/src/main/java/almapenada/daam/fragments/FriendsFragment.java
+++ app/src/main/java/almapenada/daam/fragments/FriendsFragment.java
@@ -1,26 +1,77 @@
 package almapenada.daam.fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import almapenada.daam.R;
 
 
 public class FriendsFragment extends Fragment {
 
-
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_friends,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
 
-        return v;
+        viewPager = (ViewPager) rootView.findViewById(R.id.friends_viewpager);
+        if (viewPager != null) {
+            setupViewPager(viewPager);
+        }
+
+        final TabLayout tabLayout = (TabLayout) rootView.findViewById(R.id.friends_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return rootView;
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter ad = new Adapter(getFragmentManager());
+        ad.addFragment(new FrirndsListFragment(),"Friends");
+        ad.addFragment(new GroupsFragment(),"Groups");
+        viewPager.setAdapter(ad);
+    }
+
+    static class Adapter extends FragmentStatePagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
+        }
+    }
+
 }
---
+++ app/src/main/java/almapenada/daam/fragments/FrirndsListFragment.java
@@ -0,0 +1,32 @@
+package almapenada.daam.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import almapenada.daam.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FrirndsListFragment extends Fragment {
+
+
+    public FrirndsListFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_frinds_list, container, false);
+
+
+        return rootView;
+    }
+
+}
