df1d7de5cbad30c4c51046f098df34aee153a34a
Daniela Costa <danielacosta31@hotmail.com>
2016-05-11 19:05:03 +0100

    ProfileFragment

--- app/src/main/java/almapenada/daam/fragments/ProfileFragment.java
+++ app/src/main/java/almapenada/daam/fragments/ProfileFragment.java
@@ -1,20 +1,40 @@
 package almapenada.daam.fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import almapenada.daam.R;
 
 public class ProfileFragment extends Fragment {
+
+    private ProfileFragment self= this;
+    private Button image;
+    private static final int SELECT_IMAGE = 1;
+    private ViewPager viewPager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
-    }
 
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        image = (Button) rootView.findViewById(R.id.image);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "BLABLABLA", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        return rootView;
+    }
 }
