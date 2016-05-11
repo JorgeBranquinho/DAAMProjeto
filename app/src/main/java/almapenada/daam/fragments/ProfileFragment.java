8596fe77e307120c1c2155e09821eda6884dec9f
Daniela Costa <danielacosta31@hotmail.com>
2016-05-12 00:31:07 +0100

    Profile

--- app/src/main/java/almapenada/daam/fragments/ProfileFragment.java
+++ app/src/main/java/almapenada/daam/fragments/ProfileFragment.java
@@ -1,6 +1,15 @@
 package almapenada.daam.fragments;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
@@ -19,6 +28,7 @@ public class ProfileFragment extends Fragment {
     private Button image;
     private static final int SELECT_IMAGE = 1;
     private ViewPager viewPager;
+    private String filePath="";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,11 +40,44 @@ public class ProfileFragment extends Fragment {
         image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(), "BLABLABLA", Toast.LENGTH_LONG).show();
+                //Toast.makeText(getActivity(), "BLABLABLA", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);//
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_IMAGE);
             }
         });
 
 
         return rootView;
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SELECT_IMAGE) {
+            if (resultCode == getActivity().RESULT_OK) {
+                if (data != null) {
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                    Cursor cursor = getActivity().getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    filePath = cursor.getString(columnIndex);
+                    cursor.close();
+                    Bitmap yourSelectedImage = BitmapFactory.decodeFile(filePath);
+                    if(yourSelectedImage != null) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {//se a API é 15+
+                            image.setBackground(new BitmapDrawable(getContext().getResources(), yourSelectedImage));
+                        }
+                    }
+                } else if (resultCode == getActivity().RESULT_CANCELED) {
+                    Toast.makeText(getActivity(), "Cancelled", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
