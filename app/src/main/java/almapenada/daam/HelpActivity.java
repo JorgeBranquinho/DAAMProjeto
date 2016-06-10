41dcab7290cf0a6d660a497db2c4bea99665fb0a
André Carvalho <andrempc@gmail.com>
2016-06-10 23:10:50 +0100

    hallo3

--- app/src/main/java/almapenada/daam/HelpActivity.java
+++ app/src/main/java/almapenada/daam/HelpActivity.java
@@ -1,19 +1,10 @@
 package almapenada.daam;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -27,18 +18,18 @@ public class HelpActivity extends AppCompatActivity {
 
         TextSliderView textSliderView = new TextSliderView(this);
         textSliderView
-                .description("Tutorial parte 1")
-                .image(R.drawable.add_img);
+                .description("Game of Thrones")
+                .image("http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
 
         TextSliderView textSliderView2 = new TextSliderView(this);
         textSliderView2
-                .description("Tutorial parte 2")
-                .image(R.drawable.logo);
+                .description("Hannibal")
+                .image("http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
 
         TextSliderView textSliderView3 = new TextSliderView(this);
         textSliderView3
-                .description("Tutorial parte 3")
-                .image(R.drawable.temp_fista);
+                .description("Porno XXX xD")
+                .image("http://cdn3.nflximg.net/images/3093/2043093.jpg");
 
         sliderShow.addSlider(textSliderView);
         sliderShow.addSlider(textSliderView2);
@@ -51,23 +42,6 @@ public class HelpActivity extends AppCompatActivity {
     protected void onStop() {
         sliderShow.stopAutoCycle();
         super.onStop();
-    }
-
-    private File drawableToFile(int id){
-        try{
-            File f=new File("file name");
-            InputStream inputStream = getResources().openRawResource(id);
-            OutputStream out=new FileOutputStream(f);
-            byte buf[]=new byte[1024];
-            int len;
-            while((len=inputStream.read(buf))>0)
-                out.write(buf,0,len);
-            out.close();
-            inputStream.close();
-            return f;
-        }
-        catch (IOException e){}
-        return null;
     }
 
 }
