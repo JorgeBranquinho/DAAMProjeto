7a79601909fe962dc11ee504364c55a96c338b5d
JorgeBranquinho <jmabo@iscte.pt>
2016-06-11 20:08:50 +0100

    /-\|_|\/|/-\|>€|\|/-\|)/-\

--- app/src/main/java/almapenada/daam/fragments/EventListFragment.java
+++ app/src/main/java/almapenada/daam/fragments/EventListFragment.java
@@ -1,6 +1,7 @@
 package almapenada.daam.fragments;
 
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -26,8 +37,10 @@ public class EventListFragment extends Fragment {
 
     private View rootView;
     private EventAdapter adapter;
-    private ArrayList<Event> events_to_display =new ArrayList<Event>();
-    private ArrayList<Event> full_event_list =new ArrayList<Event>();
+    private ArrayList<Event> events_to_display = new ArrayList<Event>();
+    private ArrayList<Event> full_event_list = new ArrayList<Event>();
+    private ArrayList<Event> temp_events = new ArrayList<Event>();
+    private ArrayList<Integer> owned_events = new ArrayList<>();
     SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
     private ListView event_list;
     private EventsDatabase database;
@@ -37,12 +50,13 @@ public class EventListFragment extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_event_list, container, false);
 
         database = new EventsDatabase(getActivity());
-        if(database.isEmpty())
+        if (database.isEmpty())
             database.populateWithExample();
 
         populateEventList(database.getAllEvents());
+        populateEventListRemote();
 
-        adapter=new EventAdapter(getActivity(), full_event_list);
+        adapter = new EventAdapter(getActivity(), full_event_list);
 
         event_list = (ListView) rootView.findViewById(R.id.event_list);
         event_list.setAdapter(adapter);
@@ -50,24 +64,81 @@ public class EventListFragment extends Fragment {
         return rootView;
     }
 
+    private void populateEventListRemote() {
+        new DownloadEvents().execute();
+    }
+
+    class DownloadEvents extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            String response = "";
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpResponse httpResponse = httpclient.execute(new HttpGet("https://daamservices-daam.rhcloud.com/getEventById/1"));
+                BufferedReader reader = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent(), "UTF-8"));
+                response = reader.readLine();
+                temp_events.add(jsonToEvent(response));
+            } catch (Exception e) {
+                //e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            full_event_list = temp_events;
+            orderByRecent();
+        }
+
+        private Event jsonToEvent(String response) {
+            JSONObject jobj = null;
+            try {
+                jobj = new JSONObject(response);
+                if (jobj.get("status").toString().compareTo("OK") == 0) {
+                    JSONArray poi = jobj.getJSONArray("teste");
+                    database = new EventsDatabase(getActivity());
+                    for (int i = 0; i < poi.length(); i++) {
+                        JSONObject t_poi = poi.getJSONObject(i);
+                        if (!owned_events.contains(Integer.parseInt(t_poi.getString("id")))) {
+                            Event x = new Event(Integer.parseInt(t_poi.getString("id")), t_poi.getString("eventName"), Integer.parseInt(t_poi.getString("isPublic")) == 1 ? true : false, t_poi.getString("weekDay"), t_poi.getString("date"), Integer.parseInt(t_poi.getString("isEndDate")) == 1 ? true : false, t_poi.getString("endDate"), Integer.parseInt(t_poi.getString("isPrice")) == 1 ? true : false, t_poi.getString("price"), t_poi.getString("hours"), Integer.parseInt(t_poi.getString("isLocation")) == 1 ? true : false, t_poi.getString("location_latlng"), Integer.parseInt(t_poi.getString("isFriendsInvitable")) == 1 ? true : false, false, false);
+                            database.insertEvent(x);
+                            full_event_list.add(x);
+                        }
+                    }
+                    database.close();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
     private void populateEventList(Cursor cursor) {
-        if (cursor .moveToFirst()) {
+        if (cursor.moveToFirst()) {
             while (cursor.isAfterLast() == false) {
                 Event e = (new EnumDatabase()).cursorToEvent(cursor);
                 full_event_list.add(e);
+                owned_events.add(e.getId());
                 cursor.moveToNext();
             }
         }
     }
 
-    public void orderByRecent(){
-        events_to_display= (ArrayList<Event>) full_event_list.clone();//TODO: verificar se isto ainda esta atual com a DB
-        if(events_to_display.size()==0)return;
+    public void orderByRecent() {
+        events_to_display = (ArrayList<Event>) full_event_list.clone();//TODO: verificar se isto ainda esta atual com a DB
+        if (events_to_display.size() == 0) return;
         Collections.sort(events_to_display, new Comparator<Event>() {
             @Override
             public int compare(Event event2, Event event1) {
                 try {
-                    return format.parse(event1.getDate()).before(format.parse(event2.getDate()))?1:-1;
+                    return format.parse(event1.getDate()).before(format.parse(event2.getDate())) ? 1 : -1;
                 } catch (ParseException e) {
                     e.printStackTrace();
                 }
@@ -75,7 +146,7 @@ public class EventListFragment extends Fragment {
                 return -1;
             }
         });
-        if(adapter==null)adapter=new EventAdapter(getActivity(), events_to_display);
+        if (adapter == null) adapter = new EventAdapter(getActivity(), events_to_display);
         event_list.setAdapter(adapter);
     }
 
@@ -84,43 +155,43 @@ public class EventListFragment extends Fragment {
     }
 
     public void orderByCheaper() {
-        events_to_display= (ArrayList<Event>) full_event_list.clone();//TODO: verificar se isto ainda esta atual com a DB
-        if(events_to_display.size()==0)return;
+        events_to_display = (ArrayList<Event>) full_event_list.clone();//TODO: verificar se isto ainda esta atual com a DB
+        if (events_to_display.size() == 0) return;
         Collections.sort(events_to_display, new Comparator<Event>() {
             @Override
             public int compare(Event event2, Event event1) {
                 if (event1.getPrice().equals("")) return 1;
                 if (event2.getPrice().equals("")) return -1;
-                return Integer.parseInt(String.valueOf(event2.getPrice()))<Integer.parseInt(String.valueOf(event1.getPrice()))?-1:1;
+                return Integer.parseInt(String.valueOf(event2.getPrice())) < Integer.parseInt(String.valueOf(event1.getPrice())) ? -1 : 1;
             }
         });
-        adapter=new EventAdapter(getActivity(), events_to_display);
+        adapter = new EventAdapter(getActivity(), events_to_display);
         event_list.setAdapter(adapter);
     }
 
     public void orderByGoing() {
-        events_to_display= (ArrayList<Event>) full_event_list.clone();//TODO: verificar se isto ainda esta atual com a DB
-        if(events_to_display.size()==0)return;
+        events_to_display = (ArrayList<Event>) full_event_list.clone();//TODO: verificar se isto ainda esta atual com a DB
+        if (events_to_display.size() == 0) return;
         ListIterator<Event> iter = events_to_display.listIterator();
-        while(iter.hasNext()){
-            if(!iter.next().isGoing()){
+        while (iter.hasNext()) {
+            if (!iter.next().isGoing()) {
                 iter.remove();
             }
         }
-        adapter=new EventAdapter(getActivity(), events_to_display);
+        adapter = new EventAdapter(getActivity(), events_to_display);
         event_list.setAdapter(adapter);
     }
 
     public void orderByNotGoing() {
-        events_to_display= (ArrayList<Event>) full_event_list.clone();//TODO: verificar se isto ainda esta atual com a DB
-        if(events_to_display.size()==0)return;
+        events_to_display = (ArrayList<Event>) full_event_list.clone();//TODO: verificar se isto ainda esta atual com a DB
+        if (events_to_display.size() == 0) return;
         ListIterator<Event> iter = events_to_display.listIterator();
-        while(iter.hasNext()){
-            if(iter.next().isGoing()){
+        while (iter.hasNext()) {
+            if (iter.next().isGoing()) {
                 iter.remove();
             }
         }
-        adapter=new EventAdapter(getActivity(), events_to_display);
+        adapter = new EventAdapter(getActivity(), events_to_display);
         event_list.setAdapter(adapter);
     }
 }
